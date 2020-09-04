abe45305491ee86484628c9a238a5bdba60bea15
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-09-04 15:18:55 -0400

    Announce abstain votes immediately

--- src/main/java/io/github/haykam821/werewolf/game/role/action/AbstainAction.java
+++ src/main/java/io/github/haykam821/werewolf/game/role/action/AbstainAction.java
@@ -3,12 +3,18 @@ package io.github.haykam821.werewolf.game.role.action;
 import io.github.haykam821.werewolf.game.PlayerEntry;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
 
 public class AbstainAction extends Action {
 	@Override
 	public void use(PlayerEntry user) {
 		super.use(user);
 		user.getPhase().addAbstainVote();
+
+		Text message = new TranslatableText(this.getTranslationKey() + ".select", user.getName()).formatted(Formatting.DARK_GREEN);
+		user.getPhase().sendMessage(message);
 	}
 
 	@Override
