ebfdd4542af0e4d7d48b60497cfab4dcf2cfb035
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-03-15 23:04:22 -0400

    Update minigame to Minecraft 1.18.2

--- src/main/java/io/github/haykam821/werewolf/Main.java
+++ src/main/java/io/github/haykam821/werewolf/Main.java
@@ -7,17 +7,17 @@ import io.github.haykam821.werewolf.game.role.RolesCommand;
 import io.github.haykam821.werewolf.game.role.action.ActionsCommand;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
-import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.block.Block;
-import net.minecraft.tag.Tag;
+import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import xyz.nucleoid.plasmid.game.GameType;
 
 public class Main implements ModInitializer {
 	public static final String MOD_ID = "werewolf";
 
 	private static final Identifier EXTINGUISHABLE_CAMPFIRES_ID = new Identifier(MOD_ID, "extinguishable_campfires");
-	public static final Tag<Block> EXTINGUISHABLE_CAMPFIRES = TagFactory.BLOCK.create(EXTINGUISHABLE_CAMPFIRES_ID);
+	public static final TagKey<Block> EXTINGUISHABLE_CAMPFIRES = TagKey.of(Registry.BLOCK_KEY, EXTINGUISHABLE_CAMPFIRES_ID);
 
 	private static final Identifier WEREWOLF_ID = new Identifier(MOD_ID, "werewolf");
 	public static final GameType<WerewolfConfig> WEREWOLF_GAME_TYPE = GameType.register(WEREWOLF_ID, WerewolfConfig.CODEC, WerewolfWaitingPhase::open);
